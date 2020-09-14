b4b435aab8027df9b89a6741f5507b57db5f62dd
Jonathan Yudi Gunawan <jonathan.gunawan@xtremax.com>
2020-09-14 16:56:14 +0700

    [deploy] fix variable

--- app.py
+++ app.py
@@ -40,7 +40,6 @@ if __name__ == "__main__":
     app.run(debug=False)
 
 if __name__ != "__main__":
-    global PATH
     gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
