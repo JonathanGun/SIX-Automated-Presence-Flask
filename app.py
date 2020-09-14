5e00aea9616d55432f739f7fb662f593c2c88b88
Jonathan Yudi Gunawan <jonathan.gunawan@xtremax.com>
2020-09-14 17:26:48 +0700

    try to fix deploy

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ if __name__ != "__main__":
     gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    PATH = "/app/vendor/geckodriver/geckodriver"
+    PATH = "/usr/local/bin:/usr/bin:/bin:/app/vendor/"
